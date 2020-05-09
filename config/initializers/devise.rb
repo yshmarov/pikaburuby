6dc1cfa5515ac1d9758866a66787d52dfaa8eff5
Yaro Shm <yashm@outlook.com>
2020-05-09 20:50:04 +0000

    gem omniauth-google-oauth2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :google_oauth2, '417931000705-gsauus9uj5t3gd473rpfmsj6hv29e32h.apps.googleusercontent.com', 'cNLu4V5miyiRll_w36fDWIYI', {}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
