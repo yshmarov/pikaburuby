c962596c5fa7a795d3d986f5958ddac70abc1372
Yaro Shm <yashm@outlook.com>
2020-05-09 21:47:57 +0000

    basic authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -13,6 +14,11 @@ class PostsController < ApplicationController
   end
 
   def edit
+    if @post.user_id == current_user.id
+      #OK
+    else
+      redirect_to root_path, notice: 'У вас нет прав!'
+    end
   end
 
   def create
@@ -30,37 +36,39 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    if @post.user_id == current_user.id
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+          format.json { render :show, status: :ok, location: @post }
+        else
+          format.html { render :edit }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to root_path, notice: 'У вас нет прав!'
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+    if @post.user_id == current_user.id
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to root_path, notice: 'У вас нет прав!'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :content)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :posts
-  root 'static_pages#index'
+  root 'posts#index'
   get 'static_pages/index'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
