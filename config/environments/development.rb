4c9f1349ce9a5dd4e23cf456008d7fc4a553a04a
Yaro Shm <yashm@outlook.com>
2020-05-09 19:37:09 +0000

    gem devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.hosts << "51bc9c6e29b049c3b71ba6ce3577cbf1.vfs.cloud9.us-east-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
